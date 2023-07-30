79524f8cef125dae1b51b387acf58b7a5aa99989
Fawn <sarahmargaret@ymail.com>
2023-07-30 19:06:08 -0400

    added model js for reaction, thought, and user

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model} = require('mongoose');
+const Reaction = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now, 
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true 
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema)
+
+module.exports = Thought
+
