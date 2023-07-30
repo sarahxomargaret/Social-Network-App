9e6a67f80ade5c2431ffcc17747f03e6bcef9cdb
Fawn <sarahmargaret@ymail.com>
2023-07-30 17:26:36 -0400

    added user routes and thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+const {
+    getThoughts,
+    getOneThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    removeReaction,
+} = require ()
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const {
+    getUser,
+    createUser,
+    getSingleUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require()
